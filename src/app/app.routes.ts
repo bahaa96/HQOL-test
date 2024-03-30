3afe7e384abb740919101b47b08e33315019f16f
Ahmed Bahaa <geekbahaa1@gmail.com>
2024-03-30 02:32:05 +0200

    Adding initial routes structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NotFoundComponent } from "./not-found/not-found.component";
+import {HomeComponent} from "./home/home.component";
+import {NewUserProfileComponent} from "./new-user-profile/new-user-profile.component";
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'user-profiles',
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./user-profile-list/user-profile-list.component')
+            .then(m => m.UserProfileListComponent),
+      },
+      {
+        path: 'new',
+        loadComponent: () =>
+          import('./new-user-profile/new-user-profile.component')
+            .then(m => NewUserProfileComponent),
+      },
+      {
+        path: 'edit/:profileId',
+        loadComponent: () =>
+          import('./edit-user-profile/edit-user-profile.component')
+            .then(m => m.EditUserProfileComponent),
+      },
+      {
+        path: 'view/:profileId',
+        loadComponent: () =>
+          import('./user-profile-details/user-profile-details.component')
+            .then(m => m.UserProfileDetailsComponent),
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
