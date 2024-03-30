6c0a0892195aad398e2337b75d3e4fed55126f4e
Ahmed Bahaa <geekbahaa1@gmail.com>
2024-03-30 02:31:08 +0200

    Adding new user profile page UI

---
+++ src/app/new-user-profile/new-user-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-user-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-user-profile.component.html',
+  styleUrl: './new-user-profile.component.css'
+})
+export class NewUserProfileComponent {
+
+}
